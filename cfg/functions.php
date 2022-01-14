2d0b473cdc90d231702c2197ae96226985fe2e62
iaminsleep <xenydok1292@gmail.com>
2022-01-14 21:25:25 +0300

    Реализованы все функции (получение, редактирование, удаление постов) вместе с ответами от сервера

--- cfg/functions.php
+++ cfg/functions.php
@@ -3,7 +3,7 @@
 require 'config.php';
 
 function getGoods($db) {
-  $posts = mysqli_query($db, "SELECT * FROM `goods`");
+  $posts = mysqli_query($db, "SELECT * FROM `goods`;");
 
   $postsList = [];
 
@@ -15,8 +15,77 @@ function getGoods($db) {
 }
 
 function getGood($db, $id) {
-  $post = mysqli_query($db, "SELECT * FROM `goods` WHERE `id` = '$id'");
-  $post = mysqli_fetch_assoc($post); /* Преобразование в обычный ассоциативный массив */
+  $post = mysqli_query($db, "SELECT * FROM `goods` WHERE `id` = '$id';");
   
-  echo json_encode($post);
+  if(mysqli_num_rows($post) === 0) {
+    http_response_code(404);
+    $res = [
+      "status" => false,
+      "message" => "Post not found"
+    ];
+    echo json_encode($res);
+  }
+  else {
+    $post = mysqli_fetch_assoc($post); /* Преобразование в обычный ассоциативный массив */
+    echo json_encode($post);
+  }
+}
+
+function addGood($db, $data) {
+
+  $name = $data['name'];
+  $description = $data['description'];
+  $category = $data['category'];
+  $gender = $data['gender'];
+  $price = $data['price'];
+  $img = $data['img'];
+  $label = $data['label'];
+  $offer = $data['offer'];
+
+  mysqli_query($db, "INSERT INTO `goods` (`id`, `name`, `description`, `category`, `gender`, `price`, `img`, `label`, `offer`) VALUES (NULL, '$name', '$description', '$category', '$gender', '$price', '$img', '$label', '$offer');");
+
+  http_response_code(201);
+
+  $res = [
+    "status" => true,
+    "good_id" => mysqli_insert_id($db)
+  ];
+
+  echo json_encode($res);
+}
+
+function updateGood($db, $data, $goodId) {
+
+  $name = $data['name'];
+  $description = $data['description'];
+  $category = $data['category'];
+  $gender = $data['gender'];
+  $price = $data['price'];
+  $img = $data['img'];
+  $label = $data['label'];
+  $offer = $data['offer'];
+
+  mysqli_query($db, "UPDATE `goods` SET `name` = '$name', `description` = '$description', `category` = '$category', `gender` = '$gender', `price` = '$price', `img` = '$img', `label` = '$label', `offer` = '$offer' WHERE `goods`.`id` = '$goodId';");
+
+  http_response_code(200);
+
+  $res = [
+    "status" => true,
+    "message" => "Good has been updated successfully"
+  ];
+
+  echo json_encode($res);
+}
+
+function deleteGood($db, $goodId) {
+  mysqli_query($db, "DELETE FROM `goods` WHERE `goods`.`id` = '$goodId';");
+
+  http_response_code(200);
+
+  $res = [
+    "status" => true,
+    "message" => "Good was successfully deleted"
+  ];
+
+  echo json_encode($res);
 }
--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Creditentials: true');
 header('Content-Type: application/json');
 
 require_once 'cfg/config.php';
 require_once 'cfg/functions.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
 
 $query = $_GET['q'];
 
@@ -12,14 +18,38 @@ $params = explode('/', $query); /* Запрос к определённому т
 $type = $params[0];
 $id = $params[1];
 
-if($type === 'goods') {
+switch($method) {
+  case 'GET':
+    if($type === 'goods') {
+      if(isset($id)) {
+        getGood($database, $id);
+      }
 
-  if(isset($id)) {
-    getGood($database, $id);
-  }
-
-  else {
-    getGoods($database);  /* Получение всех товаров */
-  }
-
+      else {
+        getGoods($database);  /* Получение всех товаров */
+      }
+    }
+    break;
+  case 'POST':
+    if($type === 'goods') {
+      addGood($database, $_POST);
+    }
+    break;
+  case 'PATCH':
+    if($type === 'goods') {
+      if(isset($id)) {
+        $data = file_get_contents('php://input');
+        $data = json_decode($data, true); /* преобразование json в обычный ассоциативный php массив, 
+        потому что метод PATCH не поддерживает form-дату из метода POST */
+        updateGood($database, $data, $id);
+      }
+    }
+    break;
+  case 'DELETE':
+    if($type === 'goods') {
+      if(isset($id)) {
+        deleteGood($database, $id);
+      }
+    }
+    break;
 }
