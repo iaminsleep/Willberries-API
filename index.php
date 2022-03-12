9f54005b46034ae307910154c8a36ce56172e10c
iaminsleep <xenydok1292@gmail.com>
2022-03-12 21:37:06 +0300

    change access control allow origin

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://willberries.herokuapp.com");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Credentials: true");
